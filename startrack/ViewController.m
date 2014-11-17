f3c9c10f76792bc2a8646abded125397be90785d
Chris Boyle <chris@bbck.net>
2014-11-17 10:28:02 -0500

    Picker view for selecting predefined targets

--- startrack/ViewController.m
+++ startrack/ViewController.m
@@ -9,19 +9,37 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    NSArray *_targetPickerData;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _targetPickerData = @[@"", @"Zero", @"North", @"South", @"East", @"West"];
+    self.targetPicker.dataSource = self;
+    self.targetPicker.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return _targetPickerData.count;
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return _targetPickerData[row];
 }
 
 @end
