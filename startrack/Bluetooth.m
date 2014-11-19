ac0035a74079bfa4065802ed2f7f9246b596ecae
Chris Boyle <chris@bbck.net>
2014-11-19 11:15:26 -0500

    Working bluetooth connection

--- startrack/Bluetooth.m
+++ startrack/Bluetooth.m
@@ -12,25 +12,27 @@
 
 - (id)init
 {
-    self.myCentralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil options:nil];
+    _myCentralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil options:nil];
     return self;
 }
 
 - (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI {
     NSLog(@"Found peropheral %@", peripheral.description);
-//    [self.myCentralManager connectPeripheral:peripheral options:nil];
+    _peripheral = peripheral;
+    [_myCentralManager connectPeripheral:_peripheral options:nil];
 }
 
 - (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral {
-    peripheral.delegate = self;
-    NSLog(@"Peripheral connected");
+    [_myCentralManager stopScan];
+    _peripheral.delegate = self;
+    [_peripheral discoverServices:nil];
 }
 
 - (void)centralManagerDidUpdateState:(CBCentralManager *)central {
     NSLog(@"centralManagerDidUpdateState");
-    switch (self.myCentralManager.state) {
+    switch (central.state) {
         case CBCentralManagerStatePoweredOn:
-            [self.myCentralManager scanForPeripheralsWithServices:@[[CBUUID UUIDWithString: @"46010CB0-8FC5-43B1-9FF8-0C65E88DCF34"]] options:nil];
+            [_myCentralManager scanForPeripheralsWithServices:@[[CBUUID UUIDWithString: @"46010CB0-8FC5-43B1-9FF8-0C65E88DCF34"]] options:nil];
             break;
             
         case CBCentralManagerStatePoweredOff:
@@ -42,6 +44,19 @@
             
         default:
             break;
+    }
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error {
+    for (CBService *service in peripheral.services) {
+        NSLog(@"Discovered service %@", service);
+        [_peripheral discoverCharacteristics:nil forService:service];
+    }
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(CBService *)service error:(NSError *)error {
+    for (CBCharacteristic *characteristic in service.characteristics) {
+        NSLog(@"Discovered characteristic %@", characteristic);
     }
 }
 
