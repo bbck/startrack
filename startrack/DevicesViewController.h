25b4f60133d48db19e1c389ca7336857cf823d74
Chris Boyle <chris@bbck.net>
2015-02-14 20:15:36 -0500

    Add table view on left side for devices.

---
+++ startrack/DevicesViewController.h
@@ -0,0 +1,13 @@
+//
+//  DevicesViewController.h
+//  StarTrack
+//
+//  Created by Chris Boyle on 2/14/15.
+//  Copyright (c) 2015 UMass Amherst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DevicesViewController : UITableViewController
+
+@end
