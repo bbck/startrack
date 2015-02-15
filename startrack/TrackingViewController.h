d46a2e3b0c79e4dce587558b0e1a14dbfae11145
Chris Boyle <chris@bbck.net>
2015-02-14 21:40:33 -0500

    Tracking view reimplemented

---
+++ startrack/TrackingViewController.h
@@ -0,0 +1,13 @@
+//
+//  TrackingViewController.h
+//  StarTrack
+//
+//  Created by Chris Boyle on 2/14/15.
+//  Copyright (c) 2015 UMass Amherst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrackingViewController : UIViewController
+
+@end
---
+++ startrack/TrackingViewController.m
@@ -0,0 +1,47 @@
+//
+//  TrackingViewController.m
+//  StarTrack
+//
+//  Created by Chris Boyle on 2/14/15.
+//  Copyright (c) 2015 UMass Amherst. All rights reserved.
+//
+
+#import "TrackingViewController.h"
+
+@interface TrackingViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *rightAscension;
+@property (weak, nonatomic) IBOutlet UITextField *declination;
+@property (weak, nonatomic) IBOutlet UIStepper *exposureCount;
+@property (weak, nonatomic) IBOutlet UITextField *exposureCountField;
+@property (weak, nonatomic) IBOutlet UIButton *startButton;
+
+@end
+
+@implementation TrackingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)exposureCountAction:(id)sender {
+    self.exposureCountField.placeholder = [NSString stringWithFormat:@"%.f", self.exposureCount.value];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
